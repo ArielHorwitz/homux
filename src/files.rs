b7e2ae6f727b0b0954d02e059423f013874052a4
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-04-09 18:02:05 +0300

    Implement apply

---
+++ src/files.rs
@@ -0,0 +1,61 @@
+use anyhow::{Context, Result};
+use std::fs;
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, Clone, Default)]
+pub struct RecursiveDirContents {
+    pub dirs: Vec<PathBuf>,
+    pub files: Vec<PathBuf>,
+}
+
+pub fn walk_dir(path: &Path) -> Result<RecursiveDirContents> {
+    let mut contents = RecursiveDirContents::default();
+    let entries =
+        fs::read_dir(path).with_context(|| format!("failed to read path: {}", path.display()))?;
+    for entry in entries {
+        let path = entry
+            .with_context(|| format!("failed to read path: {}", path.display()))?
+            .path();
+        if path.is_dir() {
+            contents.dirs.push(path.clone());
+            let mut subcontents = walk_dir(&path)?;
+            contents.dirs.append(&mut subcontents.dirs);
+            contents.files.append(&mut subcontents.files);
+        } else {
+            contents.files.push(path);
+        }
+    }
+    Ok(contents)
+}
+
+pub fn get_relative_path<P: AsRef<Path>>(base: &Path, absolute_path: P) -> Result<PathBuf> {
+    Ok(absolute_path
+        .as_ref()
+        .strip_prefix(base)
+        .with_context(|| {
+            format!(
+                "{} is not relative to {}",
+                absolute_path.as_ref().display(),
+                base.display()
+            )
+        })?
+        .to_path_buf())
+}
+
+pub fn copy_directory_full<P: AsRef<Path>>(from: P, to: P) -> Result<()> {
+    let dir_contents = walk_dir(from.as_ref())?;
+    for dir_path in dir_contents.dirs {
+        let relative_path = get_relative_path(from.as_ref(), &dir_path)
+            .with_context(|| format!("non-relative path: {}", dir_path.display()))?;
+        let target_path = to.as_ref().to_path_buf().join(relative_path);
+        std::fs::create_dir_all(&target_path).context("failed to create staging subdirectory")?;
+    }
+    for file_path in dir_contents.files {
+        let relative_path = get_relative_path(from.as_ref(), &file_path)
+            .with_context(|| format!("non-relative path: {}", file_path.display()))?;
+        let target_path = to.as_ref().to_path_buf().join(relative_path);
+        std::fs::copy(&file_path, &target_path)
+            .with_context(|| format!("failed to copy to {}", target_path.display()))?;
+    }
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(name = "homux")]
+#[clap(about = "Synchronize your home directory across host machines.")]
+#[clap(author = "https://ariel.ninja")]
+#[clap(version)]
+struct Args {
+    /// Operation
+    #[command(subcommand)]
+    operation: Operation,
+}
+
+#[derive(Debug, Parser)]
+enum Operation {
+    /// Apply a source directory
+    Apply(ApplyArgs),
+}
+
+#[derive(Debug, Parser)]
+struct ApplyArgs {
+    /// Source directory
+    #[arg(short = 's', long)]
+    source_dir: Option<PathBuf>,
+    /// Use a custom hostname
+    #[arg(short = 'n', long)]
+    hostname: Option<String>,
+    /// Dry run (apply to temporary directory instead of home directory)
+    #[arg(short = 'd', long)]
+    dry_run: bool,
+    /// Show more verbose output
+    #[arg(short = 'v', long)]
+    verbose: bool,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    match args.operation {
+        Operation::Apply(args) => apply(args)?,
+    }
+    Ok(())
+}
+
+fn apply(args: ApplyArgs) -> Result<()> {
+    let source_dir = if let Some(source_dir) = args.source_dir {
+        // TODO: check if suspect that source directory is not a home directory
+        source_dir
+    } else {
+        get_default_source_directory().context("get default source directory")?
+    };
+    let target_dir = if args.dry_run {
+        PathBuf::from(String::from("/tmp/homux.dry_run"))
+    } else {
+        unimplemented!("only dry run is implemented")
+    };
+    let hostname = if let Some(hostname) = args.hostname {
+        hostname
+    } else {
+        get_machine_hostname().context("get hostname")?
+    };
+    let apply_args = homux::ApplyArgs {
+        source_dir,
+        target_dir,
+        hostname,
+        max_file_size: 1_000_000,
+        verbose: args.verbose,
+    };
+    if args.verbose {
+        println!("{apply_args:#?}");
+    }
+    homux::apply(apply_args)?;
+    Ok(())
+}
+
+fn get_default_source_directory() -> Result<PathBuf> {
+    Ok(
+        PathBuf::from(std::env::var("HOME").context("get user home directory from environment")?)
+            .join(".local/share/homux/source"),
+    )
+}
+
+fn get_machine_hostname() -> Result<String> {
+    Ok(String::from_utf8(
+        std::process::Command::new("hostnamectl")
+            .arg("hostname")
+            .output()
+            .context("failed to get hostname")?
+            .stdout,
+    )?
+    .trim()
+    .to_owned())
+}
